b5302502b3be849a06f0bd233a4c683549ca2b31
piktamine <trujoknow@gmail.com>
2022-04-22 18:25:19 -0500

    Scene fixed

--- ZombieConga/GameViewController.swift
+++ ZombieConga/GameViewController.swift
@@ -1,48 +1,20 @@
-//
-//  GameViewController.swift
-//  ZombieConga
-//
-//  Created by Admin on 29/07/21.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
 
+//por defecto cargaba el gamescene.sks
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {//Se ejeuta cuando se carga la escena
+    override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill //Escala para que ocupe toda la pantalla
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true //para las capas de visualizacion del juego
-            
-            view.showsFPS = true //muesta los fps en la parte inferior
-            view.showsNodeCount = true
-        }
+        let scene =
+        GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
